9bbe029a570b88de486ea328a278e325d87f81a1
suleymanovtat <suleymanovtat@gmail.com>
2018-02-24 21:53:05 +0300

    Created the project. I added a menu with 3 items  - Settings, Search, Exit. I added FrameLayout to the markup, which occupies the entire screen. When you click on the menu items, a toast appears with the name of the item

--- app/src/main/java/ru/suleymanovtat/secondtask/MainActivity.java
+++ app/src/main/java/ru/suleymanovtat/secondtask/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.suleymanovtat.secondtask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int nameItemMenu = R.string.app_name;
+        switch (item.getItemId()) {
+            case R.id.actionSettings:
+                nameItemMenu = R.string.settings;
+                break;
+            case R.id.actionSearch:
+                nameItemMenu = R.string.search;
+                break;
+            case R.id.actionExit:
+                nameItemMenu = R.string.exit;
+                break;
+            default:
+                break;
+        }
+        showMessage(nameItemMenu);
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showMessage(int string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
     }
 }
